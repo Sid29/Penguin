78d42da5f17e839e77dbafa50dadd2c1d206dcab
mdagniau <mdagniau@orange.fr>
2015-09-21 14:28:52 +0200

    MAJ HelloWorld !

--- penguin/src/main/java/fr/esiea/penguin/App.java
+++ penguin/src/main/java/fr/esiea/penguin/App.java
@@ -1,13 +1,24 @@
 package fr.esiea.penguin;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class, args);
+	}
 }
