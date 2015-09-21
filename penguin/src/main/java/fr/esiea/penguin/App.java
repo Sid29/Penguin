450aa52fd7f93dfd11b8d1f72aede572501bada6
mdagniau <mdagniau@orange.fr>
2015-09-21 14:56:36 +0200

    h2 integration test2

--- penguin/src/main/java/fr/esiea/penguin/App.java
+++ penguin/src/main/java/fr/esiea/penguin/App.java
@@ -1,7 +1,10 @@
 package fr.esiea.penguin;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +17,13 @@ public class App
 	String home() {
 		return "Hello World!";
 	}
+	
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(App.class, args);
