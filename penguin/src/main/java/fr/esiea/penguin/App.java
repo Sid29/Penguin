3a85d998e54b944974f0d73b59424ff9d355a93e
mdagniau <mdagniau@orange.fr>
2015-09-22 15:15:41 +0200

    Update hibernate.cfg.xml App.java : redirect home to index.html Create webapp, css, img and js folders with files

--- penguin/src/main/java/fr/esiea/penguin/App.java
+++ penguin/src/main/java/fr/esiea/penguin/App.java
@@ -1,7 +1,5 @@
 package fr.esiea.penguin;
 
-import java.util.List;
-
 import org.h2.server.web.WebServlet;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
@@ -9,30 +7,24 @@ import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
-import fr.esiea.penguin.Entity.UserEntity;
-import fr.esiea.penguin.Service.UserService;
 
 @Controller
 @EnableAutoConfiguration
 public class App 
 {
-	UserService userService;
-	
-	@RequestMapping("/")
 	@ResponseBody
-	String home() {
-		userService = new UserService();
-		List<UserEntity> users = userService.findAll();
-		return "Hello " + users.get(0).getFirstname() +"!";
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/index.html");
 	}
-	
-    @Bean
-    ServletRegistrationBean h2servletRegistration(){
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
-        registrationBean.addUrlMappings("/console/*");
-        return registrationBean;
-    }
+
+	@Bean
+	ServletRegistrationBean h2servletRegistration(){
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+		registrationBean.addUrlMappings("/console/*");
+		return registrationBean;
+	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(App.class, args);
